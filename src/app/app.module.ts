e4a71461edd07a5ba40476e1b540612be737bd96
delgadogab2713 <delgadogab2713@gmail.com>
2024-05-21 19:57:46 +0000

    Adding routing features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRouter: Routes = [
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:392407973.
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServerComponent },
+]
 
 @NgModule({
   declarations: [
@@ -25,6 +33,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
